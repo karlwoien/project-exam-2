22c00f721b7b48e7deefefe52c7c61835ddd00ed
karlwoien <karlwoien@gmail.com>
2025-01-21 17:44:01 +0100

    creates initial Layout component

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer";
+import Header from "../Header";
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <main className="flex-grow vh-100">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
