24cebaca20f2828221184099c60786b73384670c
karlwoien <karlwoien@gmail.com>
2025-01-22 14:47:30 +0100

    Update index.jsx

--- src/components/LinkButton/index.jsx
+++ src/components/LinkButton/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 
 export default function LinkButton({ to, label, variant = "primary", className = "", ...props }) {
-    const baseClass = "inline-block text-center py-2 px-4 rounded transition duration-300";
+    const baseClass = "inline-block text-center text-base py-2 px-4 rounded-full transition duration-400";
     const variantClass = {
-        primary: "bg-bg-primary text-bg-muted hover:bg-bg-highlight",
+        primary: "bg-bg-primary text-white hover:bg-bg-highlight",
         accent: "bg-bg-accent text-black hover:bg-bg-primary",
-        highlight: "bg-bg-highlight text-black hover:bg-bg-primary",
+        highlight: "bg-bg-highlight text-black border border-bg-highlight hover:bg-transparent hover:text-bg-muted",
+        muted: "bg-transparent text-bg-muted border border-bg-muted hover:bg-bg-muted hover:text-black",
     };
 
     return (
