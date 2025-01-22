53c387981bae575b695976b6b63e29fc09a020b1
karlwoien <karlwoien@gmail.com>
2025-01-22 21:39:48 +0100

    updates hover effect

--- src/components/LinkButton/index.jsx
+++ src/components/LinkButton/index.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function LinkButton({ to, label, variant = "primary", className = "", ...props }) {
     const baseClass = "inline-block text-center text-base py-2 px-4 rounded-full transition duration-400";
     const variantClass = {
-        primary: "bg-bg-primary text-white hover:bg-bg-highlight",
-        accent: "bg-bg-accent text-black hover:bg-bg-primary",
+        primary: "bg-bg-primary border border-bg-primary text-white hover:bg-transparent hover:text-black",
+        accent: "bg-bg-accent border border-bg-accent text-black hover:bg-transparent hover:text-white",
         highlight: "bg-bg-highlight text-black border border-bg-highlight hover:bg-transparent hover:text-bg-muted",
         muted: "bg-transparent text-bg-muted border border-bg-muted hover:bg-bg-muted hover:text-black",
     };
