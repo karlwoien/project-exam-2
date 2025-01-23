cbed9ec7c47897d1b93fd70120eeea91e35a804c
karlwoien <karlwoien@gmail.com>
2025-01-23 15:26:35 +0100

    creates constants file with API_URLs, API_KEY etc.

---
+++ src/api/constants.js
@@ -0,0 +1,5 @@
+export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const API_BASE_URL = "https://v2.api.noroff.dev";
+export const API_HOLIDAZE = "/holidaze";
+export const API_HOLIDAZE_URL = API_BASE_URL + API_HOLIDAZE;
