f1a4c1bae664148ff8e26b19ebe52470974b6137
karlwoien <karlwoien@gmail.com>
2025-02-03 10:52:06 +0100

    adds fallback option and max-length title

--- src/components/VenueCard/index.jsx
+++ src/components/VenueCard/index.jsx
@@ -1,4 +1,5 @@
 import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineImageNotSupported } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export default function VenueCard ({venue}) {
@@ -14,21 +15,28 @@ export default function VenueCard ({venue}) {
     if (venue.meta.breakfast) amenities.push("Breakfast");
     if (venue.meta.pets) amenities.push("Pets");
     const amenitiesText = amenities.length > 0 ? amenities.join(", ") : "No amenities listed";
-
+    
+    // Sjekk om venue har bilde
+    const hasImage = venue.media?.length > 0 && venue.media[0]?.url;
+ 
     return (
         <Link to={`venue/${venue.id}`}>
         <div className="card flex flex-col">
             {/*Image */}
-            <div className="h-[200px] w-full overflow-hidden pb-2.5">
-                <img 
-                    src={venue.media[0]?.url} 
-                    alt={venue.name}
-                    className="w-full h-full object-fill rounded-t-md"
-                />
+            <div className="h-[200px] w-full overflow-hidden pb-2.5 flex justify-center items-center">
+            {hasImage ? (
+                        <img
+                            src={venue.media[0].url}
+                            alt={venue.name}
+                            className="w-full h-full object-fill rounded-t-md"
+                        />
+                    ) : (
+                        <MdOutlineImageNotSupported className="text-gray-500 text-6xl" />
+                    )}
             </div>
             {/*Content */}
             <div>
-                <h3 className="text-xl">{venue.name}</h3>
+                <h3 className="text-xl">{venue.name.length > 25 ? `${venue.name.slice(0, 25)}...` : venue.name}</h3>
                 <div className="flex items-center space-x-2.5">
                     <CiLocationOn/>
                     <p>{location}</p>
@@ -39,4 +47,4 @@ export default function VenueCard ({venue}) {
         </div>
         </Link>
     );
-};
+}
