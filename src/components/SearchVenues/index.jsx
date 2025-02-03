4ccebd651e2ec116544e43a7bcea8ab44e5c860b
karlwoien <karlwoien@gmail.com>
2025-02-03 10:52:22 +0100

    updates styling search input

--- src/components/SearchVenues/index.jsx
+++ src/components/SearchVenues/index.jsx
@@ -12,7 +12,7 @@ export default function Search({ onSearch }) {
                 type="text"
                 placeholder="Search venues..."
                 onChange={handleInputChange}
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-bg-highlight"
+                className="w-full px-4 py-2 border border-bg-primary rounded-md focus:outline-none focus:ring-2 focus:ring-bg-highlight focus:border-transparent"
             />
         </div>
     );
