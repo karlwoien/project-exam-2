0c5265d5461058bf38bb0a813c420f823cbf13b8
karlwoien <karlwoien@gmail.com>
2025-02-05 15:26:51 +0100

    adds persist to handle local storage and update profile

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,14 +1,28 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
-  
-  // Setter brukerdata og token ved login
-  login: (userData, token) => set({ user: userData, token }),
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
 
-  // Nullstiller state ved logout
-  logout: () => set({ user: null, token: null }),
-}));
+      // Setter brukerdata og token ved login
+      login: (userData, token) => set({ user: userData, token }),
+
+      // Nullstiller state ved logout
+      logout: () => set({ user: null, token: null }),
+
+      // Oppdater brukerprofil
+      updateProfile: (updatedData) =>
+        set((state) => ({
+          user: { ...state.user, ...updatedData },
+        })),
+    }),
+    {
+      name: "auth-storage", // Lagrer brukerdata i Local Storage
+    }
+  )
+);
 
 export default useAuthStore;
