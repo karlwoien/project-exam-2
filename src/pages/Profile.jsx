bee71e8a5507335de13abf36a1a7d3f633d8dbce
karlwoien <karlwoien@gmail.com>
2025-02-05 17:19:19 +0100

    refactor code and adds useProfile hook

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,43 +1,12 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import useProfile from "../hooks/useProfile";
 import useAuthStore from "../store/authStore";
-import { fetchUserProfile, fetchUserVenues, fetchUserBookings } from "../api/apiClient";
 import ProfileBookings from "../components/Profile/ProfileBookings";
 import ProfileVenues from "../components/Profile/ProfileVenues";
 import ProfileCard from "../components/Profile/ProfileCard";
 
 export default function Profile() {
-    const { user, token, logout } = useAuthStore();
-    const [profile, setProfile] = useState(null);
-    const [venues, setVenues] = useState([]);
-    const [bookings, setBookings] = useState([]);
-    const [error, setError] = useState(null);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (!user) {
-          navigate("/login")
-        };
-        
-        async function fetchProfileData() {
-            try {
-                const profileData = await fetchUserProfile(user.name, token);
-                setProfile(profileData.data);
-
-                if (user.venueManager) {
-                    const userVenues = await fetchUserVenues(user.name, token);
-                    setVenues(userVenues.data);
-                } else {
-                    const userBookings = await fetchUserBookings(user.name, token);
-                    setBookings(userBookings.data);
-                }
-            } catch (err) {
-                setError(err.message);
-            }
-        }
-
-        fetchProfileData();
-    }, [user, token]);
+    const { logout, user } = useAuthStore();
+    const { profile, venues, bookings, error } = useProfile();
 
     if (!user) return <p className="text-center text-red-500">You must be logged in to view this page.</p>;
     if (error) return <p className="text-red-500">Error: {error}</p>;
@@ -48,11 +17,7 @@ export default function Profile() {
             {profile && <ProfileCard profile={profile} />}
 
             {/* Conditional Display */}
-            {user.venueManager ? (
-                <ProfileVenues venues={venues} />
-            ) : (
-                <ProfileBookings bookings={bookings} />
-            )}
+            {user.venueManager ? <ProfileVenues venues={venues} /> : <ProfileBookings bookings={bookings} />}
 
             {/* Logout Button */}
             <button onClick={logout} className="w-full mt-6 bg-red-500 text-white py-2 rounded-md hover:bg-red-600">
