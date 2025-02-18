5e6a1fb0826cc8627b62120ed05f5cad03cb01e9
karlwoien <karlwoien@gmail.com>
2025-02-18 19:38:45 +0100

    adds toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes'
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Importer styling
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
-};
+      <>
+          <RouterProvider router={router} />
+          <ToastContainer />
+      </>
+  );
+}
