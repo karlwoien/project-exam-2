e809b033a9b266701ec9b76678fd2b22f65b5193
karlwoien <karlwoien@gmail.com>
2025-02-18 11:59:52 +0100

    enhance booking info

--- src/components/VenueInfo/UpcomingBookings.jsx
+++ src/components/VenueInfo/UpcomingBookings.jsx
@@ -4,23 +4,26 @@ import DeleteVenue from "./DeleteVenue";
 export default function UpcomingBookings({ bookings, venueId, handleDelete, isDeleting }) {
     return (
         <div className="mb-10">
-            <h3 className="text-xl font-normal mb-2.5">Upcoming Bookings</h3>
+            <h3 className="text-xl font-normal mb-2.5">Bookings</h3>
             {bookings && bookings.length > 0 ? (
                 <ul className="space-y-4">
                     {bookings.map((booking) => (
                         <li key={booking.id} className="border p-3 rounded-md bg-gray-100">
-                            <p><strong>From:</strong> {new Date(booking.dateFrom).toLocaleDateString()}</p>
-                            <p><strong>To:</strong> {new Date(booking.dateTo).toLocaleDateString()}</p>
-                            <p><strong>Guests:</strong> {booking.guests}</p>
+                            <p className="text-sm"><strong>Customer:</strong> {booking.customer.name}</p>
+                            <p className="text-sm"><strong>Email:</strong> {booking.customer.email}</p>
+                            <div className="flex space-x-2">
+                                <p className="text-sm"><strong>From:</strong> {new Date(booking.dateFrom).toLocaleDateString()}</p>
+                                <p className="text-sm"><strong>To:</strong> {new Date(booking.dateTo).toLocaleDateString()}</p>
+                            </div>
+                            <p className="text-sm"><strong>Guests:</strong> {booking.guests}</p>
                         </li>
                     ))}
                 </ul>
             ) : (
-                <p className="text-gray-500">No bookings yet.</p>
+                <p className="text-gray-500">No bookings made yet</p>
             )}
 
             <h3 className="text-xl font-normal mb-2.5 mt-4">Manage your venue</h3>
-
             {/* Handling for Venue Manager */}
             <div className="flex space-x-3">
                 <EditVenue venueId={venueId} />
