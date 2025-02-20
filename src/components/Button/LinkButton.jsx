aecbac3e41125424d7cff947f6117ef528280408
karlwoien <karlwoien@gmail.com>
2025-02-20 13:45:25 +0100

    enhance readability on different pages

--- src/components/Button/LinkButton.jsx
+++ src/components/Button/LinkButton.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function LinkButton({ to, label, variant = "primary", className = "", ...props }) {
     const baseClass = "inline-block text-center text-base py-2 px-4 rounded-full transition duration-400";
     const variantClass = {
-        primary: "bg-bg-primary border border-bg-primary text-white hover:bg-transparent hover:text-black",
-        accent: "bg-bg-accent border border-bg-accent text-black hover:bg-transparent hover:text-white",
+        primary: "bg-bg-primary border border-transparent text-white hover:bg-bg-highlight",
+        accent: "bg-bg-accent border border-bg-accent text-black hover:brightness-90",
         highlight: "bg-bg-highlight text-black border border-bg-highlight hover:bg-transparent hover:text-white hover:scale-105 transition-transform duration-300",
         muted: "bg-transparent border border-white hover:border-bg-highlight hover:text-bg-highlight hover:scale-105 transition-transform duration-300",
     };
