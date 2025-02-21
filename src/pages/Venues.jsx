ea2342e3b6bf8c07eef876cce6d3f2c8eedad533
karlwoien <karlwoien@gmail.com>
2025-02-21 17:28:56 +0100

    refactor(venues): add JSDoc, removes inline comments

--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -4,12 +4,17 @@ import useVenues from '../hooks/useVenues';
 import { useTitle } from '../hooks/useTitle';
 import LoadingSpinner from '../components/Loading/LoadingSpinner';
 
+/**
+ * Venues page displaying a list of available venues with search functionality.
+ * @returns {JSX.Element} - Rendered Venues page.
+ */
 export default function Venues() {
   useTitle('Venues');
   const { venues, isLoading, error, fetchVenues } = useVenues();
 
   const handleSearch = (query) => {
-    fetchVenues(query.trim() || ''); // Bruk query eller hent alle venues
+    const trimmedQuery = query.trim();
+    fetchVenues(trimmedQuery);
   };
 
   if (isLoading) {
@@ -23,12 +28,10 @@ export default function Venues() {
   return (
     <div className="mx-auto max-w-5xl">
       <h1 className="mb-5 text-4xl text-bg-highlight">EXPLORE VENUES</h1>
-      {/* Search input and filter option */}
       <div className="w-full">
-        {/* Search input */}
         <VenueSearch onSearch={handleSearch} />
       </div>
-      {/* Venues grid */}
+      
       <div className="mx-auto max-w-6xl">
         <div className="grid auto-rows-fr grid-cols-1 gap-7 sm:grid-cols-2 md:grid-cols-3">
           {venues.map((venue) => (
