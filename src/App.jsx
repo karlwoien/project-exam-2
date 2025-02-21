19b92f61f66cbafefd9ce91ed990b8eb77215ef4
karlwoien <karlwoien@gmail.com>
2025-02-21 17:58:55 +0100

    removes inline comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; // Importer styling
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
